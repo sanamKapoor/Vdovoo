9a930d9ffbe83e5cf28503d7e605ade9deb6c529
sanamKapoor <sanamkapoor3285@gmail.com>
2022-05-23 07:52:50 +0530

    Updates

---
+++ precache-manifest.c14bef5928414a1fb93d6198809aaa4c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb4dfaef5a23d94435b908bda6b98a9",
+    "url": "/Vdovoo/index.html"
+  },
+  {
+    "revision": "c5be3f5bbe092ec2193b",
+    "url": "/Vdovoo/static/css/main.56b595c8.chunk.css"
+  },
+  {
+    "revision": "e97f3c3f84cdf7d7b247",
+    "url": "/Vdovoo/static/js/0.a4298a21.chunk.js"
+  },
+  {
+    "revision": "04569601696e18435e90",
+    "url": "/Vdovoo/static/js/3.dc906a49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Vdovoo/static/js/3.dc906a49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01eeeb9df863acbea668",
+    "url": "/Vdovoo/static/js/4.8da0d805.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/Vdovoo/static/js/4.8da0d805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6af5e2a21bbd90e683",
+    "url": "/Vdovoo/static/js/5.c0a26321.chunk.js"
+  },
+  {
+    "revision": "bb6a7d5f5dc8725d4eff",
+    "url": "/Vdovoo/static/js/6.9462747e.chunk.js"
+  },
+  {
+    "revision": "92c55cd1fb0db1e012ce",
+    "url": "/Vdovoo/static/js/7.da31b974.chunk.js"
+  },
+  {
+    "revision": "844e6c5ef2ba328578c5",
+    "url": "/Vdovoo/static/js/8.fa28c110.chunk.js"
+  },
+  {
+    "revision": "c5be3f5bbe092ec2193b",
+    "url": "/Vdovoo/static/js/main.ae964df7.chunk.js"
+  },
+  {
+    "revision": "96fdceec168386180dd0",
+    "url": "/Vdovoo/static/js/runtime-main.cf721016.js"
+  },
+  {
+    "revision": "6d18dc91fdcb7c3638353164a446a49d",
+    "url": "/Vdovoo/static/media/6e1cea410afc3f0a5ed9312570c21d98.6d18dc91.jpg"
+  }
+]);
